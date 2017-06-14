0844db71966f203ddd71a49d90cc40d0898d3926
Susan <susanfuhn@gmail.com>
2017-06-14 10:59:01 +0000

    Generating Model

---
+++ app/models/banking.rb
@@ -0,0 +1,2 @@
+class Banking < ActiveRecord::Base
+end
---
+++ db/migrate/20170614105024_create_bankings.rb
@@ -0,0 +1,10 @@
+class CreateBankings < ActiveRecord::Migration
+  def change
+    create_table :bankings do |t|
+      t.date :Date
+      t.string :Title
+      t.float :Amount
+      t.timestamps null: false
+    end
+  end
+end
