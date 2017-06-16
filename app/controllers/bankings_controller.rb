2a8a436c27e61991ac00097ce324f8f1283aa512
Susan <susanfuhn@gmail.com>
2017-06-16 10:28:31 +0000

    Form to enter the record

--- app/controllers/bankings_controller.rb
+++ app/controllers/bankings_controller.rb
@@ -1,4 +1,15 @@
 class BankingsController < ApplicationController
-def index
-end
+    def index
+        @bankings = Banking.all
+    end
+    
+    def create
+        @banking = Banking.create(banking_params)
+        redirect_to root_path
+    end
+    
+    private 
+    def banking_params
+       params.require(:banking).permit(:Date, :Title, :Amount) 
+    end
 end
